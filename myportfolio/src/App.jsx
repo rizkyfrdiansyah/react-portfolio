ce48f767f5d1029c592bac738c1e7745650c2786
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2024-01-20 15:21:29 +0700

    feat: components/hero

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import { Hero } from "./components/Navbar/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className={styles.App}>
         <Navbar />
+        <Hero />
       </div>
     </>
   );
